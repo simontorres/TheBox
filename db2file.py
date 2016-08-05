4f4c9b5c257a80884da81e078d5c7ea1e43b0b40
Simon Torres <storres@ctio.noao.edu>
2016-08-05 17:54:33 -0400

    Added argparse and updated documentation

--- db2file.py
+++ db2file.py
@@ -1,27 +1,31 @@
 #!/usr/local/python
-"""CCD Reduction Routine for e2v camera at Swope Telescope
+"""Tool to DUMP TheBox MySQL Database to an ASCII File
 
-This pipeline performs all the necessary reduction processes for single nights
+This script access the MySQL Database and retrieve the data
+
+Note:
+    This script complies with PEP8 and PEP257 (Google Style)
+
 
 """
 
 import os
 import sys
-import time
-import datetime
+# import time
+# import datetime
 import argparse
 import getpass
 import base64
-import cPickle as pickle
+import cPickle
 # import mysql.connector
 # from mysql.connector import (connection)
 # from mysql.connector import errorcode
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import matplotlib.patches as mpatches
-import socket
-import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.gridspec as gridspec
+# import matplotlib.patches as mpatches
+# import socket
+# import pandas as pd
 import logging as log
 import warnings
 
@@ -34,16 +38,84 @@ __version__ = "0.1"
 __email__ = "storres@ctio.noao.edu"
 __status__ = "Development"
 
+
 class MainApp:
     """Dumps database table into a Comma Separated Values file
 
     """
 
     def __init__(self):
-        self.access_file_name = 'mysqlaccess'
-        self.thebox_env_dir = os.path.expanduser('~/') + '.thebox/'
+        self.args = self.get_args()
+        self.access_file_name = self.args.my_file
+        self.thebox_env_dir = os.path.expanduser('~/') + self.args.thebox_dir
         self.access = self.define_environment()
-        self.my_connection = connection.MySQLConnection()
+        # self.my_connection = connection.MySQLConnection()
+
+    @staticmethod
+    def get_args():
+        """Handles the argparse library and returns the arguments
+
+
+
+        Returns:
+            args (class): Contains all the arguments parsed or default values
+
+        """
+
+        parser = argparse.ArgumentParser(description='Tool to dump TheBox MySQL Database to ASCII File.')
+
+        parser.add_argument('--mysql-access-file',
+                            action='store',
+                            default='mysqlaccess',
+                            type=str,
+                            dest='my_file',
+                            help='Name of the file that stores access information.')
+
+        parser.add_argument('--default-dir',
+                            action='store',
+                            default='.thebox/',
+                            type=str,
+                            dest='thebox_dir',
+                            help='Location of TheBox Files.')
+
+        parser.add_argument('-U', '--my-user',
+                            action='store',
+                            default='thebox',
+                            dest='my_user',
+                            help='MySQL User Name.')
+
+        parser.add_argument('-H', '--my-host',
+                            action='store',
+                            default='localhost',
+                            dest='my_host',
+                            help='MySQL Host Name.')
+
+        parser.add_argument('-D', '--my-database',
+                            action='store',
+                            default='theBoxData',
+                            dest='my_database',
+                            help='MySQL Database Name.')
+        """
+
+        parser.add_argument('--skip-bias',
+                            action='store_false',
+                            default=True,
+                            dest='bias',
+                            help='Skip bias correction. Helpful when you want to repeat later processes')
+
+        parser.add_argument('--skip-linearity',
+                            action='store_false',
+                            default=True,
+                            dest='linearity',
+                            help='Skip linearity correction. Helpful when you want to repeat later processes')
+        """
+
+        args = parser.parse_args()
+
+        if args.thebox_dir[-1] != '/':
+            args.thebox_dir += '/'
+        print args
+        return args
 
     def define_environment(self):
         """Performs System checks and performs necessary actions
@@ -57,23 +129,24 @@ class MainApp:
 
         """
         if os.path.isdir(self.thebox_env_dir):
-            log.debug("Directory exist %s",self.thebox_env_dir)
+            log.debug("Directory exist %s", self.thebox_env_dir)
             if os.path.isfile(self.thebox_env_dir + self.access_file_name):
-                log.debug("Access file exists")
+                log.debug("Access file exists %s", self.thebox_env_dir + self.access_file_name)
                 i_file = open(self.thebox_env_dir + self.access_file_name, 'rb')
-                access_data = pickle.load(i_file)
+                access_data = cPickle.load(i_file)
                 print(access_data)
             else:
                 log.debug("access File doesnt exist")
                 access_data = self.access_info_request()
         else:
-            log.debug("Creating directory %s",self.thebox_env_dir)
+            log.debug("Creating directory %s", self.thebox_env_dir)
             try:
                 os.path.os.mkdir(self.thebox_env_dir)
                 log.debug("Directory created sucessfully")
                 access_data = self.access_info_request()
             except OSError as error:
                 log.debug(error)
+                sys.exit(0)
 
         return access_data
 
@@ -86,20 +159,18 @@ class MainApp:
 
         """
         log.warning("There is no environment defined please answer the questions.")
-        my_user = 'thebox'
-        my_password = base64.b64encode(getpass.getpass(prompt="Password (%s): "%my_user))
-        my_host = 'localhost'
-        my_database = 'theBoxData'
-        access_data = {'user' : my_user,
-                       'password' : my_password,
-                       'host': my_host,
-                       'database' : my_database}
+        my_password = base64.b64encode(getpass.getpass(prompt="Password (%s): " % self.args.my_user))
+        access_data = {'user': self.args.my_user,
+                       'password': my_password,
+                       'host': self.args.my_host,
+                       'database': self.args.my_database}
         my_file_name = self.thebox_env_dir + self.access_file_name
         o_file = open(my_file_name, 'wb')
-        pickle.dump(access_data, o_file, protocol=pickle.HIGHEST_PROTOCOL)
+        cPickle.dump(access_data, o_file, protocol=cPickle.HIGHEST_PROTOCOL)
         o_file.close()
+        print access_data
         return access_data
+
 
 if __name__ == '__main__':
     App = MainApp()
-    # os.rmdir('/user/simon/.thebox/')
