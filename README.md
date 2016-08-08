9885790b31d88255af5891a0d6229172d2ca192f
Simon Torres <simonneftali@gmail.com>
2016-08-08 17:46:28 -0400

    Implemented output to CSV format and updated readme file and documentation

--- README.md
+++ README.md
@@ -8,6 +8,43 @@ TheBox Experiment at the University of Goettingen, Germany.
 ### db2file.py
 This scripts reads the MySQL database in which TheBox logs all the
 temperature data. This information is written to a text file
-**IN DEVELOPMENT** is the option to choose the output file format. by
-default it will be CSV.
+by default it writes to **_output.csv_** (i.e. to a comma separated value format)
+It is possible to generate outputs in other formats but it will be done
+on request.
+
+Example:
+    The first time you run this script you will be prompted to enter the password for the database. Then it will
+    be stored in the user's home directory in a hidden folder. It is possible to parse as arguments all the
+    parameters for a given MySQL database such as: user, password, host, database and table.
+
+        $ python2.7 db2file.py
+
+   In the following example almost all the options are used even though all the values are the default ones
+
+        $ python2.7 db2file.py --default-dir .thebox
+                               --mysql-access-file mysqlaccess
+                               --my-user thebox
+                               --my-host localhost
+                               --my-database theBoxData
+                               --my-table temperatureAndStatusPhaseTwo
+                               --output-name output
+                               --output-format .csv
+                               --get-all
+                               --header
+
+   The options thar are not shown in the previous example are:
+   
+        -h or --help
+        --update-password
+
+   There are also short versions available for MySQL specific arguments.
+   
+        -U --my-user
+        -H --my-host
+        -D --my-database
+        -T --my-table
+
+Note:
+    This script complies with [PEP8](https://www.python.org/dev/peps/pep-0008/) and [PEP257](https://www.python.org/dev/peps/pep-0257/) ([Google Style Docstrings](http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html))
+ 
 Software Specific for TheBox Experiment
--- db2file.py
+++ db2file.py
@@ -1,34 +1,60 @@
 #!/usr/local/python
-"""Tool to DUMP TheBox MySQL Database to an ASCII File
+"""Tool to extract TheBox MySQL Database to an ASCII File
 
-This script access the MySQL Database and retrieve the data
+This script access the MySQL Database, retrieves the data and write a csv file by default. It is possible to
+write the output in a different format but it will be done on request.
 For backwards compatibility the old mysql-python is used instead of the latest MySQLdb
+
+Example:
+    The first time you run this script you will be prompted to enter the password for the database. Then it will
+    be stored in the user's home directory in a hidden folder. It is possible to parse as arguments all the
+    parameters for a given MySQL database such as: user, password, host, database and table.
+
+        $ python2.7 db2file.py
+
+    In the following example almost all the options are used even though all the values are the default ones
+
+        $ python2.7 db2file.py --default-dir .thebox
+                               --mysql-access-file mysqlaccess
+                               --my-user thebox
+                               --my-host localhost
+                               --my-database theBoxData
+                               --my-table temperatureAndStatusPhaseTwo
+                               --output-name output
+                               --output-format .csv
+                               --get-all
+                               --header
+
+    The options thar are not shown in the previous example are:
+        -h or --help
+        --update-password
+
+    There are also short versions available for MySQL specific arguments.
+        -U --my-user
+        -H --my-host
+        -D --my-database
+        -T --my-table
 
 Note:
     This script complies with PEP8 and PEP257 (Google Style)
-
 
 """
 
 import os
 import sys
-# import time
-# import datetime
 import argparse
 import getpass
 import base64
 import pickle
 import mysql.connector
 from mysql.connector import (connection)
-# import MySQLdb.connections as connection
 from mysql.connector import errorcode
-# import socket
-# import pandas as pd
+from pandas import DataFrame
 import logging as log
 import warnings
 
 warnings.filterwarnings('ignore')
-log.basicConfig(level=log.DEBUG)
+log.basicConfig(level=log.INFO)
 
 __author__ = 'Simon Torres'
 __date__ = '2016-08-04'
@@ -41,6 +67,18 @@ class MainApp:
     """Dumps database table into a Comma Separated Values file
 
     """
+    table_columns = ['DATE',
+                     'SENSOR_ONE',
+                     'SENSOR_TWO',
+                     'SENSOR_THREE',
+                     'SENSOR_FOUR',
+                     'SENSOR_FIVE',
+                     'SENSOR_SIX',
+                     'SENSOR_SEVEN',
+                     'TH_SET_TEMP',
+                     'TH_VAL_TEMP',
+                     'TH_HEATER_ON',
+                     'AIR_HEATER_ON']
 
     def __init__(self):
         self.args = self.get_args()
@@ -49,9 +87,9 @@ class MainApp:
         self.access = self.define_environment()
         self.my_connection = self.connect_mysql()
         self.cursor = self.my_connection.cursor()
-        # self.query_constraints = self.get_constraints()
-        # self.data = self.mysql_query()
-        # self.write_to_file()
+        self.query_constraints = self.get_constraints()
+        self.data = self.mysql_query(self.query_constraints)
+        self.write_to_file()
 
     @staticmethod
     def get_args():
@@ -77,7 +115,7 @@ class MainApp:
                             action='store_true',
                             default=False,
                             dest='set_new_password',
-                            help='Request a new Passwords')
+                            help='Request a new Password.')
 
         parser.add_argument('--default-dir',
                             action='store',
@@ -104,11 +142,37 @@ class MainApp:
                             dest='my_database',
                             help='MySQL Database Name.')
 
-        parser.add_argument('-T', '--table-name',
+        parser.add_argument('-T', '--my-table',
                             action='store',
                             default='temperatureAndStatusPhaseTwo',
                             dest='table',
                             help='MySQL Table Name')
+
+        parser.add_argument('--output-name',
+                            action='store',
+                            default='output',
+                            dest='output_name',
+                            help='Output File Name')
+
+        parser.add_argument('--output-format',
+                            action='store',
+                            default='.csv',
+                            dest='output_format',
+                            choices=['.csv'],
+                            help='Output File Name')
+
+        parser.add_argument('--get-all',
+                            action='store_false',
+                            default=True,
+                            dest='get_all',
+                            help='Get all available data.')
+
+        parser.add_argument('--header',
+                            action='store_true',
+                            default=False,
+                            dest='header',
+                            help='Include a header with column names in the output file.')
+
         """
         parser.add_argument('--skip-linearity',
                             action='store_false',
@@ -142,6 +206,8 @@ class MainApp:
                 i_file = open(self.thebox_env_dir + self.access_file_name, 'rb')
                 access_data = pickle.load(i_file)
                 # print(access_data)
+                if access_data['user'] != self.args.my_user:
+                    access_data = self.access_info_request()
             else:
                 log.debug("access File doesnt exist")
                 access_data = self.access_info_request()
@@ -191,6 +257,39 @@ class MainApp:
                 log.info("MySQL: Database does not exist")
             else:
                 log.debug(err)
+
+    def get_constraints(self):
+        query_root = 'SELECT * FROM '
+        if self.args.get_all:
+            log.info('Getting all the data from table %s', self.args.table)
+            my_query = ("""%s %s""" % (query_root, self.args.table))
+            log.debug("MySQL Query: %s", my_query)
+            return my_query
+        else:
+            log.info('Getting part of the data')
+            log.info("I'm sorry. This part in not implemented yet.")
+            return 0
+
+    def mysql_query(self, query):
+        if query != 0:
+            try:
+                self.cursor.execute(query)
+                if self.args.header:
+                    data_frame = DataFrame(self.cursor.fetchall(), columns=self.table_columns)
+                else:
+                    data_frame = DataFrame(self.cursor.fetchall())
+                self.my_connection.commit()
+                # data_frame.to_csv('everything.csv')
+                return data_frame
+            except mysql.connector.errors.InterfaceError:
+                log.debug("MySQL: raised InterfaceError")
+            except mysql.connector.errors.OperationalError:
+                log.debug("MySQL: raised OperationalError")
+
+    def write_to_file(self):
+        if self.args.output_format == ".csv":
+            print self.data.keys()
+            self.data.to_csv(self.args.output_name + self.args.output_format, )
 
 
 if __name__ == '__main__':
